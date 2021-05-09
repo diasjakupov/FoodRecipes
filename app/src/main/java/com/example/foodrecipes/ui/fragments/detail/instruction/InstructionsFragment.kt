9a99de2093968fb08d52b1a033566e80fd294b06
Dias <dias.dzhakupov.68@mail.ru>
2021-05-09 15:44:10 +0600

    commit 21: create InstructionsFragment.kt

--- app/src/main/java/com/example/foodrecipes/ui/fragments/detail/instruction/InstructionsFragment.kt
+++ app/src/main/java/com/example/foodrecipes/ui/fragments/detail/instruction/InstructionsFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import com.example.foodrecipes.R
+import com.example.foodrecipes.data.db.models.entities.RecipeResult
+import kotlinx.android.synthetic.main.fragment_instructions.view.*
 
 class InstructionsFragment : Fragment() {
 
@@ -13,8 +16,11 @@ class InstructionsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        val mView=inflater.inflate(R.layout.fragment_instructions, container, false)
+        val args: RecipeResult? =arguments?.getParcelable("recipeBundle")
+        mView.instructionWebView.webViewClient= object : WebViewClient() {}
+        args?.sourceUrl?.let { mView.instructionWebView.loadUrl(it) }
+        return mView
     }
 
 }
